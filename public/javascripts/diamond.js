9e8dd3bc863727fdff163b36a5d3a088014c9038
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2017-08-31 13:08:26 -0400

    navbar fade

--- public/javascripts/diamond.js
+++ public/javascripts/diamond.js
@@ -1,9 +1,23 @@
 $(function() {
     var showPromo = false;
-    if (!showPromo) {
-        showPromo = true;
-        $(".secondary-promo").animate({
-            opacity: 1,
-        }, 2000)
-    }
+    $(window).scroll(function() {
+        var textHeight = $(".body-text").offset().top;
+        //  console.log("TextHeight:  " + textHeight);
+        if ($(this).scrollTop() >= textHeight) {
+            if (!showPromo) {
+                showPromo = true;
+                $(".secondary-promo").animate({
+                    opacity: 1,
+                }, 1000)
+            }
+        } else {
+            if (showPromo) {
+                showPromo = !showPromo;
+                $(".secondary-promo").animate({
+                    opacity: 0,
+                }, 1000)
+            }
+        }
+    });
+
 });
