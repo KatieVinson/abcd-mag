4ea8638545733e80f05539dfa20d073c6c1d084d
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2017-09-01 13:24:41 -0400

    mobile fonts

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/honor', function(req, res) {
     res.render('honor');
 })
 app.use('/npm', express.static(path.join(__dirname, 'node_modules')));
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use('/fonts', express.static(path.join(__dirname, 'fonts')));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
